46d6c73f5879fcbb566fa383d402e69b05288df5
GeorgeLIu <g5321247@gmail.com>
2020-10-20 23:54:53 +0800

    [Update] 203. Remove Linked List Elements - iteration

--- LinkedList/203.RemoveLinkedListElements.swift
+++ LinkedList/203.RemoveLinkedListElements.swift
@@ -11,12 +11,26 @@ import Foundation
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 
-func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
-    guard let head = head else { return nil }
-    head.next = removeElements(head.next, val)
-    return head.val == val ? head.next : head
-}
+//func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+//    guard let head = head else { return nil }
+//    head.next = removeElements(head.next, val)
+//    return head.val == val ? head.next : head
+//}
 
 // Iteration
 // Time Complexity: O(N)
-// Space Complexity: O(N)
+// Space Complexity: O(1)
+func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+    var curr = head
+
+    while curr != nil {
+        let next = curr?.next
+        if next?.val == val {
+            curr?.next = next?.next
+        } else {
+            curr = next
+        }
+    }
+
+    return head?.val == val ? head?.next : head
+}
