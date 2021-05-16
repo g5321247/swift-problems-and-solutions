afa6a5116408380e32d360aa0b2e205a3aedd4d3
GeorgeLIu <g5321247@gmail.com>
2021-05-16 23:03:44 +0800

    [Add] 1009. Complement of Base 10 Integer

---
+++ BitManipulation/1009.ComplementofBase10Integer.swift
@@ -0,0 +1,34 @@
+//
+//  209. Minimum Size Subarray Sum .swift
+//  
+//
+//  Created by George Liu on 2020/10/11.
+//
+
+import Foundation
+
+/*
+因為最多只會有 32 個位數，所以最多只會跑 32 次
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    func bitwiseComplement(_ N: Int) -> Int {
+        guard N > 0 else { return 1 }
+
+        var n = N
+        var mask = 1
+
+        while n > 0 {
+            n = n >> 1
+            mask = mask << 1
+        }
+
+        /* 這邊 - 1 是因為他會多一位
+        101 的 mask 會變成 1000，減 1 會變成 111
+        */
+        mask -= 1
+        return mask ^ N
+    }
+}
