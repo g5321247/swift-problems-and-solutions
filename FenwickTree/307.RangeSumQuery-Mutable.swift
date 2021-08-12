00f68b415d2c77e3113b11146e35d694d59f9186
GeorgeLIu <g5321247@gmail.com>
2021-08-12 17:41:10 +0800

    [Update] 307. Range Sum Query - Mutable - Add Segment Tree solution

--- FenwickTree/307.RangeSumQuery-Mutable.swift
+++ FenwickTree/307.RangeSumQuery-Mutable.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 /*
+Fenwick tree
 Time complexity:
 init: O(nlogn) update: O(logn) query: O(logn)
 Space complexity: O(n)
+連結：
+https://www.youtube.com/watch?v=WbafSgetDDk&t=24s&ab_channel=HuaHua
 */
 
 class NumArray {
@@ -69,3 +72,103 @@ class FenwickTree {
 
     private func lowBit(x: Int) -> Int { return (x & -x) }
 }
+
+/*
+SegmentTree tree
+Time complexity:
+init: O(nlogn) update: O(logn) query: O(logn)
+Space complexity: O(n)
+連結:
+https://www.youtube.com/watch?v=rYBtViWXYeI&t=1323s&ab_channel=HuaHua
+*/
+
+//class NumArray {
+//
+//    let tree: SegmentTree
+//
+//    init(_ nums: [Int]) {
+//        tree = NumArray.makeTree(start: 0, end: nums.count - 1, arr: nums)
+//    }
+//
+//    func update(_ index: Int, _ val: Int) {
+//        update(index, val, node: tree)
+//    }
+//
+//    func sumRange(_ left: Int, _ right: Int) -> Int {
+//        return sumRange(left, right, tree)
+//    }
+//
+//    private func sumRange(_ left: Int, _ right: Int, _ node: SegmentTree?) -> Int {
+//        guard let node = node else { return 0 }
+//
+//        if node.start == left, node.end == right {
+//            return node.val
+//        }
+//
+//        let mid = (node.start + node.end) / 2
+//
+//        if mid > right {
+//            return sumRange(left, right, node.left)
+//        } else if mid < left {
+//            return sumRange(left, right, node.right)
+//        } else {
+//            return sumRange(left, mid, node.left) + sumRange(mid + 1, right, node.right)
+//        }
+//    }
+//
+//    private func update(_ index: Int, _ val: Int, node: SegmentTree?) {
+//        guard let node = node else { return }
+//        if node.start == index, node.end == index {
+//            node.val = val
+//            return
+//        }
+//
+//        let mid = (node.start + node.end) / 2
+//
+//        if index > mid {
+//            update(index, val, node: node.right)
+//        } else {
+//            update(index, val, node: node.left)
+//        }
+//
+//        node.val = (node.left?.val ?? 0) + (node.right?.val ?? 0)
+//    }
+//
+//    private static func makeTree(start: Int, end: Int, arr: [Int]) -> SegmentTree {
+//        if start == end { return SegmentTree(start: start, end: end, val: arr[start]) }
+//        let mid = (start + end) / 2
+//        let left = makeTree(start: start, end: mid, arr: arr)
+//        let right = makeTree(start: mid + 1, end: end, arr: arr)
+//
+//        let node = SegmentTree(start: start, end: end, val: 0)
+//        node.val = left.val + right.val
+//        node.left = left
+//        node.right = right
+//
+//        return node
+//    }
+//}
+//
+//class SegmentTree {
+//    let start: Int
+//    let end: Int
+//    var val: Int
+//
+//    var left: SegmentTree?
+//    var right: SegmentTree?
+//
+//    init(start: Int, end: Int, val: Int) {
+//        self.start = start
+//        self.end = end
+//        self.val = val
+//    }
+//}
+
+/*
+["NumArray","sumRange","update","sumRange"]
+[[[1,3,5,8,10]],[0,2],[1,2],[0,2]]
+["NumArray","sumRange"]
+[[[1,3,5,8,10]],[2,4]]
+["NumArray","sumRange"]
+[[[1,3,5,8,10]],[1,3]]
+*/
